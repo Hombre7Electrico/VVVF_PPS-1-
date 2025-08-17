89f88a8ed452d619c0dfe184f782afb5ae508c56
Hombre7Electrico <leandro.albornoz1492@gmail.com>
2025-08-16 21:43:11 -0300

    2: Diseño formulario login

--- VVVF_Historico_PPS/login.cpp
+++ VVVF_Historico_PPS/login.cpp
@@ -6,6 +6,11 @@ login::login(QWidget *parent)
     , ui(new Ui::login)
 {
     ui->setupUi(this);
+
+    QPixmap pix(":/misc/resources/01-Logo.jpg");
+    int w= ui->logo->width();
+    int h= ui->logo->height();
+    ui->logo->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 login::~login()
